28c2bdc8a40e6d4c250f3d04ef057737bc940c54
Natalie Clark <nj_clark@hotmail.co.uk>
2023-09-20 16:53:31 +0100

    Rename personal_diary to make_snippet

---
+++ tests/test_make_snippet.py
@@ -0,0 +1,11 @@
+# A function called make_snippet that takes a string 
+# as an argument and returns the first five words and 
+# then a '...' if there are more than that.
+
+"""
+If given an empty string
+It returns an empty string
+"""
+def test_given_empty_string_returns_empty_string():
+    result = make_snippet("")
+    assert result == ""
